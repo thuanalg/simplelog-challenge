dc0dfedadff54aea5a15223a97548e7da16a8556
ThuanNguyenThai <thuannt@localhost.localdomain>
2025-02-18 08:59:37 +0700

    Test more in Linux CentOS 8 cores.

--- src/linux/master_run.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=./
-./simpplelog_cpp_sample --is_master=1
---
+++ src/linux/xmaster_run.sh
@@ -0,0 +1,3 @@
+mv trigger_master.txt trigger_master_.txt
+export LD_LIBRARY_PATH=./
+./simple_cpp_example --is_master=1
