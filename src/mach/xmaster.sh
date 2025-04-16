8aadb571aca0b5d7bc01fc578eaf542cfbd74343
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-04-17 01:43:07 +0700

    add script

--- src/mach/xmaster.sh
+++ src/mach/xmaster.sh
@@ -1,4 +1,4 @@
 export DYLD_LIBRARY_PATH=./
 #./simplelog_cpp_sample --nthread=10 --loopcount=1000000
 mv trigger_master.txt trigger_master_.txt
-./simplelog_cpp_direct_sample --is_master=1
+./simple_cpp_example_direct --is_master=1
