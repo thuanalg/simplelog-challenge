5245683e215057c5ec7a9fa737f1b1563f355ba5
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-06-14 15:31:47 +0700

    Fix daily/monthly/yearly error.

--- src/linux/xfork_run.sh
+++ src/linux/xfork_run.sh
@@ -1,3 +1,3 @@
 #Note, you ONLY this file with process_mode=1
 export LD_LIBRARY_PATH=./
-./simple_c_fork --is_master=0 --loopcount=1000000 --cfg=z.cfg
+./simple_c_fork --is_master=0 --loopcount=1000000 --cfg=z.cfg --nthread=10 --topic_index=0
