d86d486a525af28bea3d129e351315244f5e61e4
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-06-14 15:27:51 +0700

    x0

--- src/linux/xslave_run.sh
+++ src/linux/xslave_run.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=./
-./simple_cpp_example --is_master=0 --loopcount=1000000 --cfg=z.cfg 
-#./simple_cpp_example --is_master=0 --loopcount=1000000 --cfg=z.cfg --topic_index=2
+./simple_cpp_example --is_master=0 --loopcount=1000000 --cfg=z.cfg --topic_index=0 --nthread=10
+#./simple_cpp_example --is_master=0 --loopcount=1000000 --cfg=z.cfg --topic_index=2 --nthread=10
